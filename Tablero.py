306821c84d45948ae54d6c0cfdce34037a43068e
DeieArti <deieartifer@gmail.com>
2024-11-12 19:10:04 +0100

    Add files via upload

--- Tablero.py
+++ Tablero.py
@@ -2,20 +2,27 @@ import numpy as np
 import variables as var
 from variables import tamanio
 
-var.tamanio
-var.barcos
-
-
 class Tablero:
 
-    def __init__(self, id_jugador, tablero_vacio, tablero_maquina, tamanio = var.tamanio, barcos= var.barcos):
+    def __init__(self, id_jugador, tablero_vacio, tamanio = var.tamanio, barcos= var.barcos):
         self.id_jugador = id_jugador
         self.tamanio = tamanio
         self.barcos = barcos
         self.tablero_vacio = tablero_vacio
-        self.tablero_maquina = tablero_maquina
+#Hemos definido tamaño y barcos como variables fijas y tablero vacio seria un array a rellenar más adelante
 
-    def posicionar_barco (self, tablero_vacio, posiciones): #revisar si hay que meter el primer self o no
+
+
+    def posicionar_barco (self, posiciones): #No hace falta meter el argumento tablero_vacio porque ya está declarado arriba
         for posicion in posiciones:
             self.tablero_vacio[posicion] = "O"
+        return self.tablero_vacio
 #Hemos definido tamaño y barcos como variables fijas y tablero vacio seria un array a rellenar más adelante
+
+
+#Comprobación del código: 
+
+hundir_flota = Tablero(876, np.full((var.tamanio,var.tamanio), " "))
+print(hundir_flota.tablero_vacio)
+posicion = hundir_flota.posicionar_barco((0,1))
+print(posicion)
