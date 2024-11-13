dbdb20562a1c0f8d04681d9df94f96f525500eba
Marpp98 <mar.p.p98@gmail.com>
2024-11-13 20:56:24 +0100

    Reunion2
    definimos la funcion disparos y creamos el bucle while para que si acertamos nos siga tocando

---
+++ Main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import variables as var
+from Tablero import Tablero
+
+
+print ("Bienvenido al juego Hundir la flota")
+print ("Las instrucciones son:") #rellenar con las instrucciones
+nombre_jugador = input ("Por favor, introduce tu nombre ")
+print(f"Hola {nombre_jugador}, ¿te apetece comenzar una partida contra mí")
+
+# Creamos los tableros del pc y del jugador
+tablero_pc = Tablero("PC", np.full((var.tamanio,var.tamanio), " "), np.full((var.tamanio,var.tamanio), " "))
+tablero_jugador = Tablero(nombre_jugador, np.full((var.tamanio,var.tamanio), " "), np.full((var.tamanio,var.tamanio), " "))
+
+'''
+barco1_pc = ([0,1])
+barco2_pc = ([(2,2),(3,2)])
+flota_pc = [barco1_pc, barco2_pc]
+
+barco1_jugador = ([1,1])
+barco2_jugador = ([(3,4),(3,5)])
+flota_jugador = [barco1_jugador,barco2_jugador]
+'''
+# Creamos todas las posiciones de forma temporal como una lista de tuplas
+flota_pc = [(0,1),(2,2),(3,2)]
+# Colocmos los barcos del pc
+posiciones_pc = tablero_pc.posicionar_barco (flota_pc)
+print(posiciones_pc)
+
+#introducimos los disparos y si aciertas te sigue tocando
+tocado = True
+while tocado:
+    disparo1 = int(input ("Por favor, introduce tu disparo X "))
+    disparo2 = int(input ("Por favor, introduce tu disparo Y "))
+    coordenadas = (disparo1,disparo2)
+    print(coordenadas)
+    disparo_jugador = tablero_pc.disparo_coordenada(coordenadas)
+    print (disparo_jugador[1])
+    tocado = disparo_jugador[2]
+    
+
--- Tablero.py
+++ Tablero.py
@@ -4,25 +4,38 @@ from variables import tamanio
 
 class Tablero:
 
-    def __init__(self, id_jugador, tablero_vacio, tamanio = var.tamanio, barcos= var.barcos):
+    def __init__(self, id_jugador, tablero_vacio, tablero_disparos, tamanio = var.tamanio, barcos= var.barcos,):
         self.id_jugador = id_jugador
         self.tamanio = tamanio
         self.barcos = barcos
         self.tablero_vacio = tablero_vacio
+        self.tablero_disparos = tablero_disparos
 #Hemos definido tamaño y barcos como variables fijas y tablero vacio seria un array a rellenar más adelante
 
 
 
-    def posicionar_barco (self, posiciones): #No hace falta meter el argumento tablero_vacio porque ya está declarado arriba
+    def posicionar_barco (self, posiciones): #No hace falta meter el argumento tablero_disparos porque ya está declarado arriba
         for posicion in posiciones:
-            self.tablero_vacio[posicion] = "O"
-        return self.tablero_vacio
+            self.tablero_disparos[posicion] = "O"
+        return self.tablero_disparos
 #Hemos definido tamaño y barcos como variables fijas y tablero vacio seria un array a rellenar más adelante
 
+    def disparo_coordenada (self, coordenada):
+        tocado = False
+        if self.tablero_disparos[coordenada] == "O":
+            self.tablero_disparos[coordenada] = "X"
+            self.tablero_vacio[coordenada] = "X"
+            tocado = True
+            print ("Tocado")
+        elif self.tablero_disparos[coordenada] == "-":
+            print ("Ya habías disparado aqui")
+        elif self.tablero_disparos[coordenada] == "X":
+            print ("Ya me habías dado")
+        else:
+            self.tablero_disparos[coordenada] = "-"
+            self.tablero_vacio[coordenada] = "-"
+            print ("Agua")
+        return [self.tablero_disparos , self.tablero_vacio, tocado]
 
-#Comprobación del código: 
-
-hundir_flota = Tablero(876, np.full((var.tamanio,var.tamanio), " "))
-print(hundir_flota.tablero_vacio)
-posicion = hundir_flota.posicionar_barco((0,1))
-print(posicion)
+    
+# Creamos la función de disparos tal y como vimos en clase
