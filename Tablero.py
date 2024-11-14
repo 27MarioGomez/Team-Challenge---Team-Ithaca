7a75260ea7ba88a085d7792677e9cf1c6620beae
27MarioGomez <mariogomezballesteros@hotmail.com>
2024-11-14 12:24:24 +0100

    update Tablero con funcion posicionar barco aleat

--- Tablero.py
+++ Tablero.py
@@ -37,4 +37,49 @@ class Tablero:
             print ("Agua")
         return [self.tablero_disparos , self.tablero_vacio, tocado]
 
-# Creamos la función de posicionar el barco aleatoriamente
+# Funcion para posicionar los barcos aleatoriamente desde el inicio
+    def posicionar_barco_aleatorio (tablero, tamanio, barcos):
+        for barco, longitud in barcos.items():
+            colocado = False
+            while not colocado:
+                orientaciones = ["N", "S", "E", "O"]
+                orientacion = random.choice(orientaciones)
+
+                # Definimos las variables para la fila inicial y para la columna inicial
+                fila_inicial = random.randint(0, tablero.shape[0] - 1)
+                columna_inicial = random.randint(0, tablero.shape[1] - 1)
+                posiciones = []
+                for i in range(longitud):
+                    if orientacion == "N":
+                        fila = fila_inicial - i # Lo restamos para movernos al norte
+                        columna = columna_inicial
+                    elif orientacion == "S":
+                        fila = fila_inicial + i
+                        columna = columna_inicial
+                    elif orientacion == "E":
+                        fila = fila_inicial
+                        columna = columna_inicial + i
+                    elif orientacion == "O":
+                        fila = fila_inicial
+                        columna = columna_inicial - i
+                    
+                    # Comprobamos si la nueva posición está dentro del tablero
+                    if 0 <= fila < tablero.shape[0] and 0 <= columna < tablero.shape[1]:
+                        posiciones.append((fila, columna))
+                    else:
+                        break
+
+                    # Comprobamos si cuando generamos un barco aleatoriamente, no pisemos a otro
+                    # Verifica si el barco tiene la longitud correcta
+                    if len(posiciones) == longitud:
+                        # Comprueba si todas las posiciones están vacías
+                        posiciones_vacias = True
+                        for pos in posiciones:
+                            if tablero[pos] != " ":
+                                posiciones_vacias = False
+                                break
+                                
+                        # Si todas las posiciones están vacías, posiciona el barco y termina el bucle
+                        if posiciones_vacias:
+                            posicionar_barco(tablero, posiciones)
+                            break
