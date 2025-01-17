ec4dd6894d046a8fe43918c7e8f37157cc8664d4
Marpp98 <mar.p.p98@gmail.com>
2025-01-17 20:56:29 +0100

    elección del dataset, creacion de notebook y archivo.py
    elección del dataset, creacion de notebook y archivo.py

---
+++ Toolbox/toolbox_ML.py
@@ -0,0 +1,12 @@
+def describe_df(): 
+    print(rellenar)
+
+def tipifica_variables():
+
+def get_features_num_regression():
+
+def plot_features_num_regression():
+
+def get_features_cat_regression():
+
+def plot_features_cat_regression():
