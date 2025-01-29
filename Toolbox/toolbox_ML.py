12d61860a54f4533c53c0214f5e20c464f08e43b
Marpp98 <mar.p.p98@gmail.com>
2025-01-29 17:48:43 +0100

    update

--- Toolbox/toolbox_ML.py
+++ Toolbox/toolbox_ML.py
@@ -7,17 +7,23 @@ from scipy.stats import pearsonr, f_oneway, kruskal, shapiro
 
 # Definir la función describe_df
 def describe_df(df):
+
     '''
     Genera un resumen detallado de un DataFrame, mostrando información clave para cada columna. 
     
-    Argumentos: df (pd.DataFrame): El DataFrame a describir. 
-    Retorna: pd.DataFrame: Un DataFrame con las siguientes métricas en las filas por cada columna: 
+    Argumentos: 
+        - df (pd.DataFrame): El DataFrame a describir. 
+
+    Retorna: 
+        - pd.DataFrame: Un DataFrame con las siguientes métricas en las filas por cada columna: 
         - Tipo de Dato: Tipo de datos de la columna (int, float, object, etc.). 
         - Porcentaje de Valores Nulos: Proporción de valores faltantes en la columna. 
         - Valores Únicos: Número de valores únicos presentes en la columna. 
         - Porcentaje de Cardinalidad: Proporción de valores únicos en relación al total de filas.  
+
     '''
-    # Inicializamos el diccionario para almacenar los resultados
+
+    # Inicializa el diccionario para almacenar los resultados
     description = {
         'Tipo de Dato': [],
         'Porcentaje de Valores Nulos': [],
@@ -25,7 +31,7 @@ def describe_df(df):
         'Porcentaje de Cardinalidad': []
     }
     
-    # Iteramos sobre las columnas del dataframe
+    # Itera sobre las columnas del dataframe
     for column in df.columns:
         # Tipo de dato
         description['Tipo de Dato'].append(df[column].dtype)
@@ -42,7 +48,7 @@ def describe_df(df):
         cardinality_percentage = (unique_values / len(df)) * 100
         description['Porcentaje de Cardinalidad'].append(cardinality_percentage)
     
-    # Convertimos el diccionario en un DataFrame
+    # Convierte el diccionario en un DataFrame
     description_df = pd.DataFrame(description, index=df.columns).T
     
     return description_df
@@ -50,16 +56,17 @@ def describe_df(df):
 
 
 def tipifica_variables(df, umbral_categoria, umbral_continua):
+
     '''
-    Analizar las columnas de un DataFrame de pandas y clasifica cada una según su tipo de datos, 
+    Analiza las columnas de un DataFrame de pandas y clasifica cada una según su tipo de datos, 
     basándose en reglas predefinidas relacionadas con la cardinalidad (cantidad de valores únicos) de las variables. 
     Genera como salida un nuevo DataFrame con el nombre de cada columna y su tipo sugerido.
 
     Argumentos:
-        - df:
-        - umbral_categoria: define el número máximo de valores únicos (cardinalidad) para clasificar una columna 
+        - df (pd.Dataframe): dataframe de pandas.
+        - umbral_categoria (int): define el número máximo de valores únicos (cardinalidad) para clasificar una columna 
             como una variable categórica. Si la cardinalidad es menor a este valor pero mayor que 2, se clasifica como categórica.
-        - umbral_continua: define el porcentaje mínimo de cardinalidad (número de valores únicos dividido entre la cantidad 
+        - umbral_continua (float): define el porcentaje mínimo de cardinalidad (número de valores únicos dividido entre la cantidad 
             total de filas) para clasificar una columna como numérica continua. 
             Las columnas que no cumplan con este criterio se clasifican como numéricas discretas.
 
@@ -68,8 +75,8 @@ def tipifica_variables(df, umbral_categoria, umbral_continua):
         - nombre_variable: Nombre de cada columna del DataFrame original.
         - tipo_sugerido: Tipo sugerido para la variable (Binaria, Categórica, Numérica Continua, o Numérica Discreta).
     
-    
     '''
+
     # Crear un DataFrame vacío para guardar los resultados
     tipo_sugerido = []
 
@@ -99,16 +106,6 @@ def tipifica_variables(df, umbral_categoria, umbral_continua):
     
     return resultado_df
 
-# Definir umbrales
-#umbral_categoria = 10  # Puedes ajustar este valor según tus necesidades
-#umbral_continua = 0.1   # Puedes ajustar este valor según tus necesidades
-
-# Aplicar la función
-#resultado = tipifica_variables(df, umbral_categoria, umbral_continua)
-
-# Mostrar el resultado
-#print(resultado)
-
 
 
 def get_features_num_regression(df, target_col, umbral_corr, pvalue = None):
@@ -119,13 +116,13 @@ def get_features_num_regression(df, target_col, umbral_corr, pvalue = None):
     la variable deberá ser mayor o igual a 1 - pvalue. 
 
     Argumentos:
-    df (pd.DataFrame): DataFrame con los datos de entrada.
-    target_col: columna objetivo que no debe aparecer en nuestra lista de variables numéricas.
-    umbral_corr: Umbral de la correlación para incluir una columna.
-    pvalue: nivel de significación para el test de correlación. Por defecto es None.
+        - df (pd.DataFrame): DataFrame con los datos de entrada.
+        - target_col (str): columna objetivo que no debe aparecer en nuestra lista de variables numéricas.
+        - umbral_corr (float): Umbral de la correlación para incluir una columna.
+        - pvalue (float or None): nivel de significación para el test de correlación. Por defecto es None.
 
     Retorna:
-    columnas_seleccionadas: lista de columnas numéricas que cumplen con los criterios de correlación.
+        - columnas_seleccionadas: lista de columnas numéricas que cumplen con los criterios de correlación.
     
     '''
 
@@ -151,10 +148,10 @@ def get_features_num_regression(df, target_col, umbral_corr, pvalue = None):
         print("Error: El valor de 'pvalue' debe ser un float entre 0 y 1.")
         return None
 
-    # selección de colunas numéricas:
+    # Selección de colunas numéricas:
     columnas_num = df.select_dtypes(include=[np.number]).columns.tolist()
 
-    # quitamos la variable target de las columnas_num si está:
+    # Quita la variable target de las columnas_num si está:
     columnas_num = [columna for columna in columnas_num if columna != target_col]
 
     #lista de columnas seleccionadas:
@@ -178,20 +175,25 @@ def get_features_num_regression(df, target_col, umbral_corr, pvalue = None):
         
     return columnas_seleccionadas
 
+
+
+
 def plot_features_num_regression(df, target_col="", columns=[], umbral_corr=0, pvalue=None):
+
     """
     Genera gráficos pairplot para analizar la relación entre las variables numéricas y una columna objetivo en un dataframe.
     También filtra y selecciona columnas en función de la correlación y significación estadística.
     
     Argumentos:
-    df (pd.DataFrame): DataFrame con los datos de entrada.
-    target_col (str): Columna objetivo para calcular correlaciones y representar gráficamente. Debe ser numérica.
-    columns (list): Lista de columnas a considerar. Si está vacía, se seleccionan automáticamente las columnas numéricas del DataFrame.
-    umbral_corr (float): Umbral mínimo de valor absoluto de la correlación para incluir una columna.
-    pvalue (float or None): Nivel de significación para el test de correlación. Si es None, no se realiza el test de significación.
+        - df (pd.DataFrame): DataFrame con los datos de entrada.
+        - target_col (str): Columna objetivo para calcular correlaciones y representar gráficamente. Debe ser numérica.
+        - columns (list): Lista de columnas a considerar. Si está vacía, se seleccionan automáticamente las columnas numéricas del DataFrame.
+        - umbral_corr (float): Umbral mínimo de valor absoluto de la correlación para incluir una columna.
+        - pvalue (float or None): Nivel de significación para el test de correlación. Si es None, no se realiza el test de significación.
 
     Retorna:
-    list: Lista de columnas que cumplen con los criterios de correlación y significación estadística.
+        - list: Lista de columnas que cumplen con los criterios de correlación y significación estadística.
+
     """
 
     # Validaciones iniciales
@@ -213,12 +215,6 @@ def plot_features_num_regression(df, target_col="", columns=[], umbral_corr=0, p
     
     # Filtrar columnas según correlación y p-value
     selected_columns = get_features_num_regression(df, target_col=target_col, umbral_corr= umbral_corr, pvalue = None)
-    '''selected_columns = []
-    for col in columns:
-        if pd.api.types.is_numeric_dtype(df[col]):
-            corr, p = pearsonr(df[col], df[target_col])
-            if abs(corr) >= umbral_corr and (pvalue is None or p < pvalue):
-                selected_columns.append(col)'''
 
     # Generar gráficos por lotes de cinco columnas
     if selected_columns:
@@ -236,49 +232,56 @@ def plot_features_num_regression(df, target_col="", columns=[], umbral_corr=0, p
 
 
 def get_features_cat_regression(df, target_col, pvalue=0.05):
+
     """
     Identifica las columnas categóricas que tienen una relación estadísticamente significativa con 
     una columna objetivo numérica en un DataFrame, usando pruebas estadísticas.
 
     Argumentos:
-    - df (pd.DataFrame): DataFrame que contiene los datos a analizar.
-    - target_col (str): Nombre de la columna objetivo que debe ser numérica.
-    - pvalue (float, opcional): Nivel de significación para las pruebas estadísticas. Debe estar entre 0 y 1. 
-    Por defecto es 0.05.
+        - df (pd.DataFrame): DataFrame que contiene los datos a analizar.
+        - target_col (str): Nombre de la columna objetivo que debe ser numérica.
+        - pvalue (float, opcional): Nivel de significación para las pruebas estadísticas. Debe estar entre 0 y 1. 
+            Por defecto es 0.05.
 
     Retorna:
-    - list: Lista de columnas categóricas que tienen una relación estadísticamente significativa con la columna 
-    objetivo.
+        - list: Lista de columnas categóricas que tienen una relación estadísticamente significativa con la columna 
+        objetivo.
 
-    Descripción general:
-    1. Valida que los argumentos sean correctos.
-    2. Identifica las columnas categóricas del DataFrame.
-    3. Realiza pruebas estadísticas (ANOVA o Kruskal-Wallis) entre la columna objetivo y las columnas categóricas.
-    4. Selecciona las columnas categóricas con valores p menores al umbral especificado.
-    
-    Notas:
-    - Usa ANOVA si los datos en los grupos tienen distribución normal, y Kruskal-Wallis si no.
-    - Maneja errores comunes, como falta de columnas categóricas o valores no válidos para `pvalue`.
     """
-    if not isinstance(df, pd.DataFrame):  #Aquí se comprueba si df es efectivamente un data frame y si no, muestra una columna de error
+    # Comprueba si df es efectivamente un data frame y si no, muestra una columna de error
+    if not isinstance(df, pd.DataFrame):  
         print("Error: 'df' debe ser un DataFrame."); return None
-    if target_col not in df.columns or not np.issubdtype(df[target_col].dtype, np.number):  #Verificamos que: target_col (la columna objetivo) exista en el DataFrame. Sea de tipo numérico. Si algo falla, mostramos un error y terminamos.
+
+    # Verifica que target_col (la columna objetivo) exista en el DataFrame y sea de tipo numérico. Si algo falla, muestra un error y termina.
+    if target_col not in df.columns or not np.issubdtype(df[target_col].dtype, np.number):  
         print(f"Error: '{target_col}' debe ser una columna numérica en el DataFrame."); return None
+
+    # Comprueba que el valor de pvalue esté entre 0 y 1 (debe ser válido para una prueba estadística). Si no cumple, para.
     if not 0 < pvalue < 1:
-        print("Error: 'pvalue' debe estar entre 0 y 1."); return None #Comprobamos que el valor de pvalue esté entre 0 y 1 (debe ser válido para una prueba estadística). Si no cumple, paramos.
+        print("Error: 'pvalue' debe estar entre 0 y 1."); return None 
     
-    categorical_columns = df.select_dtypes(include=['object', 'category']).columns   #Aquí buscamos las columnas categóricas del DataFrame, es decir, las que son de tipo object o category.
-                                                                                       #Si no encontramos ninguna columna categórica, mostramos un error y salimos.'''
+    # Busca las columnas categóricas del DataFrame, es decir, las que son de tipo object o category.
+    categorical_columns = df.select_dtypes(include=['object', 'category']).columns   
+
+
+    # Si no encuentra ninguna columna categórica, muestra un error y sale.                                                                                   
     if len(categorical_columns) == 0:
         print("Error: No hay columnas categóricas."); return None
     
     
-    relevant_columns = []    #Creamos una lista vacía llamada relevant_columns para almacenar las columnas categóricas que tengan una relación estadísticamente significativa con la columna objetivo (target_col).
+    # Crea una lista vacía llamada relevant_columns para almacenar las columnas categóricas que tengan una relación estadísticamente significativa con la columna objetivo (target_col).
+    relevant_columns = []    
     for col in categorical_columns:
-        groups = [group.dropna() for _, group in df.groupby(col)[target_col]] #Aquí se agrupan los datos de target_col por categorías de la columna actual, 
-        if len(groups) < 2 or any(group.empty for group in groups): #Para cada categoría, filtramos las filas correspondientes en el DataFrame (df[df[col] == category]) y seleccionamos la columna objetivo (target_col).
+
+        # Agrupa los datos de target_col por categorías de la columna actual.
+        groups = [group.dropna() for _, group in df.groupby(col)[target_col]]  
+
+        # Para cada categoría, filtra las filas correspondientes en el DataFrame (df[df[col] == category]) y selecciona la columna objetivo (target_col).
+        if len(groups) < 2 or any(group.empty for group in groups): 
             continue
-        if all(shapiro(g)[1] > pvalue for g in groups if len(g) >= 3):  #Mediante el código que sigue se verifica si los grupos siguen una dstribución normal
+
+        # Verifica si los grupos siguen una dstribución normal
+        if all(shapiro(g)[1] > pvalue for g in groups if len(g) >= 3):  
             test_result = f_oneway(*groups)
         else:
             test_result = kruskal(*groups)
@@ -289,21 +292,23 @@ def get_features_cat_regression(df, target_col, pvalue=0.05):
 
 
 def plot_features_cat_regression(df, target_col="", columns=None, pvalue=0.05, with_individual_plot=False):
+
     """
     Pinta histogramas agrupados para variables significativas y devuelve las variables que cumplen el criterio estadístico.
     
     Argumentos:
-    - df (pd.dataframe): df que contiene los datos.
-    - target_col (str): Nombre de la columna objetivo. Debe ser numérica continua.
-    - columns (list): Lista de variables categóricas a analizar. Si es None, se seleccionarán todas las categóricas del df.
-    - pvalue (float): Nivel de significación estadística para considerar una relación como significativa.
-    - with_individual_plot (bool): Si True, generará gráficos individuales además de los agrupados.
+        - df (pd.dataframe): df que contiene los datos.
+        - target_col (str): Nombre de la columna objetivo. Debe ser numérica continua.
+        - columns (list): Lista de variables categóricas a analizar. Si es None, se seleccionarán todas las categóricas del df.
+        - pvalue (float): Nivel de significación estadística para considerar una relación como significativa.
+        - with_individual_plot (bool): Si True, generará gráficos individuales además de los agrupados.
 
     Return:
-    - selected_features (list): Lista de columnas que tienen una relación significativa con la columna objetivo.
+        - selected_features (list): Lista de columnas que tienen una relación significativa con la columna objetivo.
+
     """
     
-    # 1.
+
     # Check de entradas
 
     # Se comprueba que el df sea un pd df
@@ -338,39 +343,15 @@ def plot_features_cat_regression(df, target_col="", columns=None, pvalue=0.05, w
             print("Error: Ninguna de las columnas proporcionadas es categórica o no está en el DataFrame.")
     
 
-    # 2. Selección de features
-    # Esto borrarlo y poner el resultado de la funcion anterior
     # Selección de features
     selected_features = get_features_cat_regression(df=df, target_col=target_col,pvalue=pvalue)
-    '''selected_features = []
     
-    for col in columns:
-        if df[col].nunique() <= 1:
-            continue  # Saltar columnas con una única categoría
-        
-        # Test de significancia ANOVA
-        groups = [df[target_col][df[col] == category].dropna() for category in df[col].unique()]
-        try:
-            _, p_val = f_oneway(*groups)
-        except ValueError:
-            print(f"Aviso: No se puede calcular ANOVA para la columna '{col}'. Usando prueba no paramétrica.")
-            try:
-                _, p_val = kruskal(*groups)
-            except ValueError:
-                print(f"Error al calcular Kruskal para la columna '{col}'. Se omitirá.")
-                continue
-            
 
-        if p_val <= pvalue:
-            selected_features.append(col)'''
-    ####### 
-
-    # 3. Plot de las features significativas
-    # Gráfico
+    # Plot de las features significativas
 
     if selected_features:
 
-        # Verificamos si 'with_individual_plot' es True y generamos los gráficos correspondientes
+        # Verifica si 'with_individual_plot' es True y genera los gráficos correspondientes
         if with_individual_plot:
 
             for column in selected_features:
@@ -384,7 +365,7 @@ def plot_features_cat_regression(df, target_col="", columns=None, pvalue=0.05, w
                     plt.legend()
                     plt.show()
 
-        # Si 'with_individual_plot' es False, generar gráficos agrupados
+        # Si 'with_individual_plot' es False, genera gráficos agrupados
         # Si es binario el target es diferente a si es continuo
         if df[target_col].nunique() != 2:
             for column in columns:
